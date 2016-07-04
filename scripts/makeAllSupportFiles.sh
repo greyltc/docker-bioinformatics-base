bbef72d167078fc34537a01d952b49519b56581a
Grey Christoforo <grey@christoforo.net>
2016-07-04 23:19:51 +0100

    Update makeAllSupportFiles.sh

--- scripts/makeAllSupportFiles.sh
+++ scripts/makeAllSupportFiles.sh
@@ -4,14 +4,14 @@
 
 THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)
 
-rm -rf "${THIS_DIR}/../genomeSize"
-bash "$THIS_DIR/makeChromSizes.sh"
+#rm -rf "${THIS_DIR}/../genomeSize"
+#bash "$THIS_DIR/makeChromSizes.sh"
 
-rm -rf "${THIS_DIR}/../inputData"
-bash "$THIS_DIR/makeExampleData.sh"
+#rm -rf "${THIS_DIR}/../inputData"
+#bash "$THIS_DIR/makeExampleData.sh"
 
-rm -rf "${THIS_DIR}/../inputData"
-bash "$THIS_DIR/makeVPlotIndex.sh"
+#rm -rf "${THIS_DIR}/../inputData"
+#bash "$THIS_DIR/makeVPlotIndex.sh"
 
-rm -rf "${THIS_DIR}/../bowtie2Index"
+#rm -rf "${THIS_DIR}/../bowtie2Index"
 bash "$THIS_DIR/makeBT2Index.sh"
