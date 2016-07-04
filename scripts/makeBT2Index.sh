6e66c9209d199433a9a8b9cbda8c895604a87077
Grey Christoforo <grey@christoforo.net>
2016-07-04 20:30:22 +0100

    build last bt2 index

--- scripts/makeBT2Index.sh
+++ scripts/makeBT2Index.sh
@@ -15,7 +15,8 @@ fi
 
 THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-declare -a MODELS=("mm9" "mm10" "hg19" "hg38")
+#declare -a MODELS=("mm9" "mm10" "hg19" "hg38")
+declare -a MODELS=("hg38")
 
 OUT_DIR="${THIS_DIR}/../bowtie2Index"
 SOURCE_DIR="${OUT_DIR}/sourceFA"
